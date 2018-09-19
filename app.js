fbf18e754b30eddda6f00b24fa4907efeb8accfe
Sudarshan Bangalore <bsudarshan@outlook.com>
2018-09-18 23:45:24 -0400

    fixed issue.. now, another issue

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var http = require('http');
 var fs = require('fs');
+var express = require('express');
+var path = require('path');
 
-// Loading the file index.html displayed to the client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./public/index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
-});
-
-// Loading socket.io
+var app = express();
+var server = http.createServer(app);
+app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(server);
+
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+app.get('/', function(req, res){
+    res.render('index');
+});
 
 io.sockets.on('connection', function (socket, username) {
     // When the client connects, they are sent a message
@@ -20,16 +24,19 @@ io.sockets.on('connection', function (socket, username) {
     socket.on('new_user', function(username) {
         socket.username = username;
     	// The other clients are told that someone new has arrived
-    	console.log(username + ' has just connected.')
+    	console.log(username + ' has just connected.');
         socket.broadcast.emit('new_text', username + ' has just connected!');
-        socket.emit('new_text', 'welcome ' + username);
+        socket.emit('new_text', 'Welcome ' + username + '.');
     });
 
     // When a "message" is received, it's logged in the console
     socket.on('message', function (message) {
         // The username of the person who sent the message is retrieved from the session variables
-        socket.broadcast.emit('new_text', socket.username + ': ' + message);
-        socket.emit('new_text', socket.username + ': ' + message)
+        socket.broadcast.emit('new_text_user', socket.username);
+        socket.broadcast.emit('new_text', message);
+        // Then, if the person wrote the text, this will also be emitted
+        socket.emit('self_text_user', username);
+        socket.emit('self_text', message);
     });
 
     // When a client is disconnected, let's other people know
@@ -38,6 +45,5 @@ io.sockets.on('connection', function (socket, username) {
     	socket.broadcast.emit('new_text', socket.username + ' has just disconnected!');
     });
 });
-
 
 server.listen(8080);
